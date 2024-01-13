29ad2583618212be3d54b048af991dc6424eeb19
LuisLuna24 <eduarlun4@gmail.com>
2024-01-13 01:06:33 -0600

    Termine nostros y acerca de

--- js/Inicio.js
+++ js/Inicio.js
@@ -1,18 +1,6 @@
 $(document).ready(function () {
-    $("#heder_inicio").on("click", function(){
-        location.href = "index.html";
-    })
-    $("#heder_servicios").on("click", function(){
-        location.href = "Servicios.html";
-    })
-    $("#heder_productos").on("click", function(){
-        location.href = "Productos.html";
-    })
-    $("#heder_publicidad").on("click", function(){
-        location.href = "Publicidad.html";
-    })
-    $("#heder_web").on("click", function(){
-        location.href = "Web.html";
+    $("#Btn_Contacto").on("click", function(){
+        location.href = "Contacto.html";
     })
     AOS.init();
 });
